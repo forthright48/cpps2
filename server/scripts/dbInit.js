a264f70ebae23592fc486186ed841cb38b922bc8
Labib666 <ewmahadicd@gmail.com>
2019-02-20 01:14:38 +0600

    7: revert back gateway root changes

--- server/scripts/dbInit.js
+++ server/scripts/dbInit.js
@@ -40,10 +40,10 @@ async function main() {
     require('../models/gateModel');
     const Gate = mongoose.model('Gate');
 
-    const root = await Gate.findOne({_id: process.env.GATEWAY_ROOT}).exec();
+    const root = await Gate.findOne({_id: '000000000000000000000000'}).exec();
     if ( !root ) {
       const newRoot = new Gate({
-        _id: process.env.GATEWAY_ROOT,
+        _id: '000000000000000000000000',
         type: 'folder',
         ancestor: [],
         ind: 0,
