066f1484637562d51622df09eb213e98ee1e3a0c
Hasib <himuhasib@gmail.com>
2019-02-28 16:01:04 +0600

    Add feature of adding new student in classroom

--- client/src/store/modules/classroom.js
+++ client/src/store/modules/classroom.js
@@ -18,18 +18,12 @@ export default {
         async fetchClassroom(context, classroomId) {
             const response = await getClassroom(classroomId)
 
-            // Hotfix
-            for (let i = 0; i < response.data.length; i++) {
-                if (response.data[i]._id === classroomId) {
-                    context.commit('SET_CLASSROOM', response.data[i])
-                    return
-                }
-            }
+            context.commit('SET_CLASSROOM', response.data)
         },
 
-        async addNewStudentToClassroom(context, { classroomId, studentId }) {
-            console.log(`classid = ${classroomId}, studentId = ${studentId}`)
-            const response = await addStudent(classroomId, studentId)
+        async addNewStudentToClassroom(context, { classroomId, studentUsername }) {
+            console.log(`classid = ${classroomId}, studentUsername = ${studentUsername}`)
+            const response = await addStudent(classroomId, studentUsername)
         },
     },
 }
