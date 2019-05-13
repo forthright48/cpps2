23d9fab0e6aebec6e2ff7aae2089ad0548b5e8c9
Labib666 <ewmahadicd@gmail.com>
2019-05-14 03:51:37 +0600

    1: fixed permission level names

--- server/api/v1/admin.js
+++ server/api/v1/admin.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {isEmpty} = require('lodash');
+const {isAdmin} = require('middlewares/userGroup');
 
 const User = require('../../models/userModel');
 
 router.get('/admin/users', getUserList);
 router.get('/admin/admins', getAdminList);
+router.get('/admin/coaches', getCoachList);
+router.post('/admin/coaches', updateCoachList);
 
 module.exports = {
   addRouter(app) {
-    app.use('/api/v1', router);
+    app.use('/api/v1', isAdmin, router);
   },
 };
 
@@ -17,12 +20,6 @@ async function getUserList(req, res, next) {
   try {
     let skip = isEmpty(req.query.skip) ? 0 : parseInt(req.query.skip);
     let limit = isEmpty(req.query.limit) ? 10 : parseInt(req.query.limit);
-    const s = req.session;
-    if (!s.roles.includes('root')) {
-      return res.status(400).json({
-        status: 400,
-      });
-    }
     const users = await User.find({}, {ojStats: 0}).skip(skip).limit(limit).exec();
     const santizedUsers = users.map((e) => {
       e.password = undefined;
@@ -41,14 +38,8 @@ async function getAdminList(req, res, next) {
   try {
     let skip = isEmpty(req.query.skip) ? 0 : parseInt(req.query.skip);
     let limit = isEmpty(req.query.limit) ? 10 : parseInt(req.query.limit);
-    const s = req.session;
-    if (!s.roles.includes('root')) {
-      return res.status(400).json({
-        status: 400,
-      });
-    }
     const users = await User.find({
-      roles: 'root',
+      roles: 'admin',
     }, {ojStats: 0}).skip(skip).limit(limit).exec();
     const santizedUsers = users.map((e) => {
       e.password = undefined;
@@ -56,6 +47,45 @@ async function getAdminList(req, res, next) {
     });
     return res.status(200).json({
       users: santizedUsers,
+      status: 200,
+    });
+  } catch (err) {
+    return next(err);
+  }
+}
+
+async function getCoachList(req, res, next) {
+  try {
+    let skip = isEmpty(req.query.skip) ? 0 : parseInt(req.query.skip);
+    let limit = isEmpty(req.query.limit) ? 10 : parseInt(req.query.limit);
+    const users = await User.find({
+      roles: 'coach',
+    }, {ojStats: 0}).skip(skip).limit(limit).exec();
+    const santizedUsers = users.map((e) => {
+      e.password = undefined;
+      return e;
+    });
+    return res.status(200).json({
+      users: santizedUsers,
+      status: 200,
+    });
+  } catch (err) {
+    return next(err);
+  }
+}
+
+async function updateCoachList(req, res, next) {
+  try {
+    let skip = isEmpty(req.query.skip) ? 0 : parseInt(req.query.skip);
+    let limit = isEmpty(req.query.limit) ? 10 : parseInt(req.query.limit);
+    const users = await User.find({
+      roles: 'root',
+    }, {ojStats: 0}).skip(skip).limit(limit).exec();
+    for (const user of users) {
+      user.roles = ['admin', 'coach', 'user'];
+      await user.save();
+    }
+    return res.status(200).json({
       status: 200,
     });
   } catch (err) {
