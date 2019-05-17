617be1ecd3bc9f0f984108b72d050417b871ebb2
Labib666 <ewmahadicd@gmail.com>
2019-05-18 04:20:27 +0600

    3: Permission changes for classroom

--- server/api/v1/classrooms.js
+++ server/api/v1/classrooms.js
@@ -14,14 +14,14 @@ router.get('/classrooms', getClassrooms);
 router.post('/classrooms', isCoach, insertClassroom);
 
 router.get('/classrooms/:classId', getClassroom);
-router.patch('/classrooms/:classId', updateClassroom);
-router.delete('/classrooms/:classId', deleteClassroom);
+router.patch('/classrooms/:classId', isCoach, updateClassroom);
+router.delete('/classrooms/:classId', isCoach, deleteClassroom);
 
 router.get('/classrooms/:classId/leaderboard', getLeaderboard);
 router.get('/classrooms/:classId/who-solved-it', solveCountInClassroom);
 
-router.put('/classrooms/:classId/students', addStudent);
-router.delete('/classrooms/:classId/students', deleteStudent);
+router.put('/classrooms/:classId/students', isCoach, addStudent);
+router.delete('/classrooms/:classId/students', isCoach, deleteStudent);
 
 router.get('/classrooms/:classId/problemlists', getProblemLists);
 
@@ -293,6 +293,13 @@ async function deleteClassroom(req, res, next) {
       }
     ).exec();
 
+    /**
+     * TODO: remove related
+     * * contests
+     * * standings
+     * * ratings
+     */
+
     return res.status(200).json({
       status: 200,
     });
@@ -304,11 +311,24 @@ async function deleteClassroom(req, res, next) {
 async function getProblemLists(req, res, next) {
   try {
     const {classId} = req.params;
+    const {userId} = req.session;
 
     if (!classId || !isObjectId(classId)) {
       return next({
         status: 401,
         message: `classId:${classId} is not a valid objectId`,
+      });
+    }
+
+    const classroom = await Classroom.findOne({
+      _id: classId,
+      $or: [{coach: userId}, {students: userId}],
+    }).exec();
+
+    if (!classroom) {
+      return next({
+        status: 404,
+        message: `classId:${classId} not found`,
       });
     }
 
